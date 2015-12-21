e484670e605816046a1f906f327bd6b260bf94ba
Manuele Simi <manuele.simi@campagnelab.org>
2015-12-21 15:37:17 -0500

    Save the state of the app before redeploying.

--- generate.sh
+++ generate.sh
@@ -1,7 +1,13 @@
+#save the state of the app (db)
+cp -r ../Circles/.meteor .
 rm -rf ../Circles
 meteor-kitchen ~/Downloads/Circles.json ../Circles
+#restore the previous state
+rm -rf ../Circles/.meteor
+mv -f .meteor ../Circles
+rm -rf .meteor
 mv -f  ~/Downloads/Circles.json .
 cd ../Circles
+meteor add jeremy:snapsvg
 meteor --port 2000
-
 
