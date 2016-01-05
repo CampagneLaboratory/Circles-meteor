4fe5155b04b1e15c178f1078e903b1a0aad9b278
Manuele Simi <manuele.simi@campagnelab.org>
2016-01-05 12:54:31 -0500

    Use the mongo db server instead of the local database.

--- generate.sh
+++ generate.sh
@@ -1,13 +1,14 @@
 #save the state of the app (db)
-cp -r ../Circles/.meteor .
+#cp -r ../Circles/.meteor .
 rm -rf ../Circles
 meteor-kitchen ~/Downloads/Circles.json ../Circles
 #restore the previous state
-rm -rf ../Circles/.meteor
-mv -f .meteor ../Circles
-rm -rf .meteor
+#rm -rf ../Circles/.meteor
+#mv -f .meteor ../Circles
+#rm -rf .meteor
 mv -f  ~/Downloads/Circles.json .
 cd ../Circles
 meteor add jeremy:snapsvg
+export MONGO_URL=mongodb://localhost:27017/circles
 meteor --port 2000
 
