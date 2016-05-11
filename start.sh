fe61d204cf78cc16501830e0ab66b9e71b8c7a66
Manuele Simi <manuele.simi@campagnelab.org>
2016-05-11 14:06:50 -0400

    Change the start script to load settings.json in the development mode.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 cd ./webapp
-export MONGO_URL=mongodb://mas2182:mas2182@localhost:27017/circles
-meteor --port 2000
+export MONGO_URL=mongodb://mas2182:<pwd>@localhost:27017/circles
+meteor --port 2000 --settings settings.json 
 
