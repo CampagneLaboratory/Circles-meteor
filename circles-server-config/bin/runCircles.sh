4b7ed80fbb6dcb5dab5aea847835343bea0dfa55
Manuele Simi <manuele.simi@campagnelab.org>
2016-05-11 13:59:59 -0400

    Set METEOR_SETTINGS variable in runCircles.sh to load the google analytics settings.

--- circles-server-config/bin/runCircles.sh
+++ circles-server-config/bin/runCircles.sh
@@ -3,6 +3,7 @@
 export MONGO_URL='mongodb://<user>:<password>@localhost:27017/circles'
 export ROOT_URL='http://localhost'
 export PORT=80
+export METEOR_SETTINGS="$(cat /home/circles/settings.json)"
 cd ~/circles-webapp/bundle/
 pm2 start node main.js --name "circles"
 
